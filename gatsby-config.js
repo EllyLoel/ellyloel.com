f3384d3cbd767bc512e2996bf41aa5a4775ac2f4
“Elly <e.loel1@protonmail.com>
2021-08-23 14:05:41 +1000

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,43 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "My Gatsby Site",
-  },
-  plugins: [],
+	siteMetadata: {
+		siteUrl: 'https://www.yourdomain.tld',
+		title: 'My Gatsby Site',
+	},
+	plugins: [
+		{
+			resolve: `gatsby-plugin-mdx`,
+			options: {
+				extensions: [`.mdx`, `.md`],
+			},
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `notes`,
+				path: `${__dirname}/content/notes/`,
+			},
+		},
+		{
+			resolve: `gatsby-plugin-mdx`,
+			options: {
+				extensions: [`.mdx`, `.md`],
+				gatsbyRemarkPlugins: [
+					{
+						resolve: 'gatsby-remark-double-brackets-link',
+						options: {
+							titleToURLPath: `${__dirname}/resolve-url.js`,
+							stripBrackets: true,
+						},
+					},
+				],
+			},
+		},
+		`gatsby-plugin-catch-links`,
+		{
+			resolve: `gatsby-transformer-markdown-references`,
+			options: {
+				types: ['Mdx'], // or ['RemarkMarkdown'] (or both)
+			},
+		},
+	],
 };
---
+++ gatsby-node.js
@@ -0,0 +1,35 @@
+exports.createPages = async ({ actions, graphql, reporter }) => {
+	const { createPage } = actions;
+
+	const notesTemplate = require.resolve(`./src/templates/noteTemplate.js`);
+
+	const result = await graphql(`
+		{
+			allFile {
+				edges {
+					node {
+						childMdx {
+							slug
+						}
+					}
+				}
+			}
+		}
+	`);
+
+	if (result.errors) {
+		reporter.panicOnBuild(`Error while running GraphQL query.`);
+		return;
+	}
+
+	result.data.allFile.edges.forEach(({ node }) => {
+		createPage({
+			path: `notes/${node.childMdx.slug}`,
+			component: notesTemplate,
+			context: {
+				// additional data can be passed via context
+				slug: node.childMdx.slug,
+			},
+		});
+	});
+};
