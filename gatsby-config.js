40799ed90b3f274df70689fbaa77288af0aef81a
Elly Loel <hello@ellyloel.com>
2022-01-12 14:40:41 +1100

    🎉 feat: adding google and vercel analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,30 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-42TKLF934T', // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          // exclude: [""],
+        },
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `notes`,
@@ -104,6 +128,13 @@ module.exports = {
         short_name: `ellyloel`,
         start_url: `/`,
         icon: `src/images/logo.png`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-vercel',
+      options: {
+        // (optional) Prints metrics in the console when true
+        debug: false,
       },
     },
   ],
