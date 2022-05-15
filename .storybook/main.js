0354427c8017d364f455ed673c0634839fc34f68
Elly Loel <hello@ellyloel.com>
2022-05-15 12:51:25 +1000

    ✨ feat: setting up storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,13 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "storybook-addon-next-router",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
-  }
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
+};
