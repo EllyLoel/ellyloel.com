a7fe9aae6d64dafeba2ff2bad29821b5a98c2eaf
Elly Loel <hello@ellyloel.com>
2022-05-15 12:51:25 +1000

    ✨ feat: setting up storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import React from "react";
 import * as NextImage from "next/image";
+import { RouterContext } from "next/dist/shared/lib/router-context";
 
 import { globalStyles } from "../src/components/globalStyles";
 
@@ -17,6 +18,23 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
+  layout: "fullscreen",
+  backgrounds: {
+    default: "light",
+    values: [
+      {
+        name: "light",
+        value: "hsl(165, 60%, 98.8%)",
+      },
+      {
+        name: "dark",
+        value: "hsl(168 48.0% 6.5%)",
+      },
+    ],
   },
 };
 
