c06c79836e07c22827fb504f2ad5921f1979eae2
Elly Loel <hello@ellyloel.com>
2022-06-07 19:39:42 +1000

    ✨ feat: adding sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://example.com',
+  siteUrl: process.env.SITE_URL || "https://ellyloel.com",
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: "*", allow: "/" }],
