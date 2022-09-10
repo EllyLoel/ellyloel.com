af8ac14404a610941ecabb220d331d859a014e12
Elly Loel <hello@ellyloel.com>
2022-09-11 00:06:46 +1000

    📝 content: obsidian commit

--- src/js/spline.js
+++ src/js/spline.js
@@ -1,12 +1,28 @@
 import { Application } from "@splinetool/runtime";
 
-const canvas = document.getElementById("canvas3d");
-const spline = new Application(canvas);
-spline
-  .load("https://prod.spline.design/uA4Ruk690NPZEunt/scene.splinecode")
-  .then(() => {
-    spline.setZoom(0.6);
+if (window.matchMedia("(min-width: 768px)").matches) {
+  const lego = document.getElementById("lego");
+  const picture = document.querySelector("#lego > picture");
+  const image = document.querySelector("#lego > picture > img");
+  const canvas = document.createElement("canvas");
+  const paragraph = (document.createElement("p").innerText =
+    "A 3D modeled Lego Minifigure designed to look like myself.");
+  const spline = new Application(canvas);
+
+  spline
+    .load("https://prod.spline.design/uA4Ruk690NPZEunt/scene.splinecode")
+    .then(() => {
+      spline.setZoom(0.6);
+    });
+
+  const observer = new MutationObserver(() => {
     setTimeout(() => {
-      canvas.style.opacity = 1;
-    }, 1000);
+      picture.style.display = "none";
+      canvas.style.visibility = "visible";
+    }, 100);
   });
+
+  canvas.append(paragraph);
+  lego.append(canvas);
+  observer.observe(canvas, { attributes: true });
+}
