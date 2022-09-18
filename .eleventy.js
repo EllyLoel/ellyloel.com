a4a583f1ac4d068403d232eaaaefff646ab6d9bf
Elly Loel <hello@ellyloel.com>
2022-09-19 08:07:53 +1000

    📝 content: obsidian commit

--- .eleventy.js
+++ .eleventy.js
@@ -107,6 +107,7 @@ module.exports = (eleventyConfig) => {
           /https?:\/\/(?:www\.)?([-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b)*(\/[\/\d\w\.-]*)*(?:[\?])*(.+)*/gm,
           ""
         )
+        .replace(/{% \w+ ?"?/gm, "")
         .split(" ")
         .slice(0, -1)
         .join(" ");
