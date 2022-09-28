b69b6088777bb355b04923936b431de3c14ac8af
Elly Loel <hello@ellyloel.com>
2022-09-28 19:42:51 +1000

    📝 content: obsidian commit

--- utils/filters.js
+++ utils/filters.js
@@ -27,6 +27,10 @@ module.exports = {
     return markdownIt({ html: true }).render(content);
   },
 
+  removeHtml: function (content = "") {
+    return markdownIt({ html: true }).render(content);
+  },
+
   sliceCollection: function (collection, count) {
     return collection.slice(0, count || 5);
   },
