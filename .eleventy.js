85a00ace4aaa321767e27527c03b654389136634
Elly Loel <hello@ellyloel.com>
2022-10-01 23:12:38 +1000

    📝 content: obsidian commit

--- .eleventy.js
+++ .eleventy.js
@@ -166,6 +166,22 @@ module.exports = (eleventyConfig) => {
     }
   );
   eleventyConfig.addNunjucksShortcode("feedItem", function (feedItem) {
+    const stage = `<span>
+    <sl-tooltip content="${
+      feedItem.stage[0].toUpperCase() + feedItem.stage.substring(1)
+    }">
+    <sl-icon class="[ emoji ]" library="fa" name="fas-${
+      feedItem.stage === "seedling" ? "seedling" : ""
+    }${feedItem.stage === "budding" ? "spa" : ""}${
+      feedItem.stage === "evergreen" ? "tree" : ""
+    }${feedItem.stage === "draft" ? "file-pen" : ""}${
+      feedItem.stage === "complete" ? "circle-check" : ""
+    }" label="${
+      feedItem.stage[0].toUpperCase() + feedItem.stage.substring(1)
+    }"></sl-icon>
+    </sl-tooltip>
+    </span>`;
+
     return `
       <li>
         <sl-card class="[ feed-item-card ]">
@@ -173,34 +189,19 @@ module.exports = (eleventyConfig) => {
           feedItem.excerpt
             ? `
                 <div slot="header" class="[ feed-item-card-title ]">
-                  <p><a href="${feedItem.url}">${feedItem.title}</a></p>
-                  ${
-                    feedItem.stage &&
-                    `<span>
-                      <sl-tooltip content="${
-                        feedItem.stage[0].toUpperCase() +
-                        feedItem.stage.substring(1)
-                      }">
-                      <sl-icon class="[ emoji ]" library="fa" name="fas-${
-                        switch (stage) {
-                          case 'seedling':
-                            return "seedling";
-                          case "budding":
-                            re
-                      }${stage === "budding" && "spa"}${
-                      stage === "evergreen" && "tree"
-                    }${stage === "draft" && "file-pen"}${
-                      stage === "complete" && "circle-check"
-                    }" label="stage"></sl-icon>
-                      </sl-tooltip>
-                      </span>`
-                  }
+                  <p>
+                    <a href="${feedItem.url}">${feedItem.title}</a>
+                    ${feedItem.stage ? stage : ``}
+                  </p>
                 </div>
                 ${markdownLibrary.render(`${feedItem.excerpt} &#8230;`)}
               `
             : `
                 <div class="[ feed-item-card-title ]">
-                  <p><a href="${feedItem.url}">${feedItem.title}</a></p>
+                  <p>
+                    <a href="${feedItem.url}">${feedItem.title}</a>
+                    ${feedItem.stage ? stage : ``}
+                  </p>
                 </div>
               `
         }
