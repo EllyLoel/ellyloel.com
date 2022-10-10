0ae9b9b39b1343b8a3688a4de9c83191fb5b05b0
Elly Loel <hello@ellyloel.com>
2022-10-10 15:49:58 +1100

    📝 content: obsidian commit

--- src/js/shoelace.js
+++ src/js/shoelace.js
@@ -12,6 +12,7 @@ import "@shoelace-style/shoelace/dist/components/drawer/drawer.js";
 import "@shoelace-style/shoelace/dist/components/card/card.js";
 import "@shoelace-style/shoelace/dist/components/breadcrumb/breadcrumb.js";
 import "@shoelace-style/shoelace/dist/components/breadcrumb-item/breadcrumb-item.js";
+import "@shoelace-style/shoelace/dist/components/animated-image/animated-image.js";
 
 import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js";
 setBasePath(
