5d4366773fb423cc2ae3835b78dfa5bf411cce63
Elly Loel <hello@ellyloel.com>
2023-01-10 18:17:51 +1100

    📝 content: obsidian commit

--- .eleventy.js
+++ .eleventy.js
@@ -337,6 +337,11 @@ module.exports = (eleventyConfig) => {
     }
   });
 
+  process.on("unhandledRejection", (error) => {
+    console.error(error); // This prints error with stack included (as for normal errors)
+    throw error; // Following best practices re-throw error and let the process exit with error code
+  });
+
   // Copy/pass-through files
   eleventyConfig.addPassthroughCopy("public");
 
