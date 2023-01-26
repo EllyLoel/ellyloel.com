bc953a275fa3a95493ad088f59ced8036e289891
Elly Loel <hello@ellyloel.com>
2023-01-26 23:37:32 +1100

    🐛 fix: fixing FOUCE issue

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,12 @@
 const postcssJitProps = require("postcss-jit-props");
-const OpenProps = require("open-props");
+const openProps = require("open-props");
 const postcssPresetEnv = require("postcss-preset-env");
 const cssnano = require("cssnano");
 
 module.exports = {
   // only variables that are used are in the build output (treeshaking)
   plugins: [
-    postcssJitProps(OpenProps),
+    postcssJitProps(openProps),
     postcssPresetEnv({ stage: 0 }),
     cssnano({ preset: "default" }),
   ],
