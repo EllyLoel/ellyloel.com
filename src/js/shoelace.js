3dbf2468161e271d0b29f5f7cb129c5c4164a84d
Elly Loel (she/her) <hello@ellyloel.com>
2023-03-06 21:46:44 +0000

    🧹 chore: upgrading and removing dependencies

--- src/js/shoelace.js
+++ src/js/shoelace.js
@@ -1,6 +1,6 @@
 import "../css/shoelace.css";
 import "@shoelace-style/shoelace/dist/components/select/select.js";
-import "@shoelace-style/shoelace/dist/components/menu-item/menu-item.js";
+import "@shoelace-style/shoelace/dist/components/option/option.js";
 import "@shoelace-style/shoelace/dist/components/badge/badge.js";
 import "@shoelace-style/shoelace/dist/components/button/button.js";
 import "@shoelace-style/shoelace/dist/components/radio-group/radio-group.js";
