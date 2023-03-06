8b32fb9b8a79f35a9ba7cab1f666007674d183c7
Elly Loel <hello@ellyloel.com>
2023-03-07 08:49:18 +1100

    WIP

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,10 +2,11 @@ const postcssJitProps = require("postcss-jit-props");
 const openProps = require("open-props");
 const postcssPresetEnv = require("postcss-preset-env");
 const cssnano = require("cssnano");
+const postcssImport = require("postcss-import");
 
 module.exports = {
-  // only variables that are used are in the build output (treeshaking)
   plugins: [
+    postcssImport(),
     postcssJitProps(openProps),
     postcssPresetEnv({ stage: 0 }),
     cssnano({ preset: "default" }),
