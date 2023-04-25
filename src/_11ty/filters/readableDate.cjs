9e10fa20786ed543948a61e06b4893c16363e07f
Elly Loel <hello@ellyloel.com>
2023-04-25 22:36:03 +1000

    🐛 fix: fixing timezone

--- src/_11ty/filters/readableDate.cjs
+++ src/_11ty/filters/readableDate.cjs
@@ -2,7 +2,7 @@ const { DateTime } = require("luxon");
 
 module.exports = (dateObj, format, zone) => {
 	// Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
-	return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(
+	return DateTime.fromJSDate(dateObj, { zone: zone || "Australia/Melbourne" }).toFormat(
 		format || "dd LLLL yyyy"
 	);
 };
