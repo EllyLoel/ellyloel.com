6382a7a88da5ea232701da346911c8d229c6dd4f
Elly Loel <hello@ellyloel.com>
2023-04-26 17:28:44 +1000

    🐛 fix: fixing feed image issue

--- src/content/bookmarks/bookmark.11tydata.cjs
+++ src/content/bookmarks/bookmark.11tydata.cjs
@@ -1,6 +1,9 @@
 module.exports = {
 	eleventyComputed: {
 		title: (data) => data.bookmark.title,
+		created: (data) => data.bookmark.created,
+		modified: (data) => data.bookmark.modified,
+		image: (data) => data.bookmark.image,
 		eleventyNavigation: {
 			key: (data) => data.bookmark.title,
 			parent: "Bookmarks",
