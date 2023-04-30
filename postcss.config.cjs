736ab9ac3f4076fcff532b305a96f4367e339505
Elly Loel <hello@ellyloel.com>
2023-04-30 21:14:40 +1000

    ✨ feat: switching to postcss and rollup from vite

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -5,11 +5,15 @@ const cssnano = require("cssnano");
 const postcssImport = require("postcss-import");
 
 module.exports = {
+	from: "src/assets/css/style.css",
+	map: process.env.ELEVENTY_ENV !== "production",
 	// only variables that are used are in the build output (treeshaking)
 	plugins: [
 		postcssImport(),
 		postcssJitProps(openProps),
 		postcssPresetEnv({ stage: 0 }),
-		cssnano({ preset: "default" }),
+		...(process.env.ELEVENTY_ENV === "production"
+			? [cssnano({ preset: "default" })]
+			: []),
 	],
 };
--- src/_11ty/plugins/plugins.cjs
+++ src/_11ty/plugins/plugins.cjs
@@ -3,9 +3,10 @@ const pluginFavicons = require("eleventy-plugin-gen-favicons");
 const pluginInterlinker = require("@photogabble/eleventy-plugin-interlinker");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const pluginNestingToc = require("eleventy-plugin-nesting-toc");
+const pluginPostCss = require("eleventy-plugin-postcss");
+const pluginRollup = require("eleventy-plugin-rollup");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxhighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const pluginVite = require("@11ty/eleventy-plugin-vite");
 const pluginWebmentions = require("eleventy-plugin-webmentions");
 
 // Internal imports
@@ -22,24 +23,14 @@ module.exports = (eleventyConfig) => {
 		wrapper: "nav",
 		wrapperClass: "[ toc ][ recursive-flow ]",
 	});
+	eleventyConfig.addPlugin(pluginPostCss);
+	eleventyConfig.addPlugin(pluginRollup, {
+		rollupOptions: "rollup.config.js",
+		scriptGenerator: (file) =>
+			`<script src="${file}" type="module" defer></script>`,
+	});
 	eleventyConfig.addPlugin(pluginRss);
 	eleventyConfig.addPlugin(pluginSyntaxhighlight);
-	eleventyConfig.addPlugin(pluginVite, {
-		viteOptions: {
-			/**
-			 * @see https://github.com/vitejs/vite/blob/ee1a686abf69db8a4026ed5462615766f222c29a/packages/vite/src/node/constants.ts#L97
-			 */
-			assetsInclude: ["**/*.xml", "**/*.gltf"],
-
-			build: {
-				sourcemap: true,
-				manifest: true,
-				rollupOptions: {
-					external: "/pagefind/pagefind.js",
-				},
-			},
-		},
-	});
 	eleventyConfig.addPlugin(pluginWebmentions, {
 		domain: metadata.domain,
 		token: process.env.WEBMENTION_IO_API_KEY,
@@ -50,6 +41,5 @@ module.exports = (eleventyConfig) => {
 	eleventyConfig.addPlugin(require("./excerpt.cjs"));
 	eleventyConfig.addPlugin(require("./image.cjs"));
 	eleventyConfig.addPlugin(require("./markdown.cjs").plugin);
-	eleventyConfig.addPlugin(require("./postcss.cjs"));
 	eleventyConfig.addPlugin(require("./unfurl.cjs"));
 };
