b150aea1a970a0cdd84a95d675e204c4ebc53a76
Elly Loel <hello@ellyloel.com>
2023-05-11 00:21:03 +1000

    🧹 chore: linting

--- src/input/content/bookmarks/bookmark.11tydata.cjs
+++ src/input/content/bookmarks/bookmark.11tydata.cjs
@@ -1,12 +1,12 @@
 module.exports = {
 	eleventyComputed: {
-		title: (data) => data.bookmark.title,
 		created: (data) => data.bookmark.created,
-		modified: (data) => data.bookmark.modified,
-		image: (data) => data.bookmark.image,
 		eleventyNavigation: {
 			key: (data) => data.bookmark.title,
 			parent: "Bookmarks",
 		},
+		image: (data) => data.bookmark.image,
+		modified: (data) => data.bookmark.modified,
+		title: (data) => data.bookmark.title,
 	},
 };
