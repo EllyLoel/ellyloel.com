1d8a9724bea6e71c36aa4053b6d8da6f3463f92e
Elly Loel <hello@ellyloel.com>
2023-05-20 11:04:37 +1000

    🐛 fix: fixing minor issues with the theme switcher

--- src/input/js/main.js
+++ src/input/js/main.js
@@ -1,6 +1,7 @@
 import "./color-scheme.js";
 import "./theme-hue.js";
 import "./color-scheme-switcher-dialog.js";
+import "./scrollbar-width.js";
 import "./shoelace.js";
 import "./details-utils.js";
 import "./css-doodle.js";
