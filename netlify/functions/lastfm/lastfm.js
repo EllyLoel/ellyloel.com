ec57501e538ef47c4a97a3c9be896d9a4f56b0ad
Elly Loel <hello@ellyloel.com>
2023-12-19 08:51:31 +1100

    🤞 fixing netlify function

--- netlify/functions/lastfm/lastfm.js
+++ netlify/functions/lastfm/lastfm.js
@@ -1,18 +1,17 @@
-const fetch = require("node-fetch");
-
-const handler = async (event) => {
+export const handler = async (event) => {
 	try {
 		const res = await fetch(
 			`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${process.env.LASTFM_USER}&api_key=${process.env.LASTFM_API_KEY}&format=json&limit=1`
 		);
 		const data = await res.json();
 		return {
-			statusCode: 200,
 			body: JSON.stringify(data),
+			statusCode: 200,
 		};
 	} catch (error) {
-		return { statusCode: 500, body: error.toString() };
+		return {
+			body: error.toString(),
+			statusCode: 500,
+		};
 	}
 };
-
-module.exports = { handler };
