af6b9b287ebd2aabdae3ebc9052a6ac903e05ee3
Elly Loel <hello@ellyloel.com>
2024-04-05 14:49:27 +1100

    Reworking unfurl styles and functionality

--- src/_11ty/plugins/unfurl.cjs
+++ src/_11ty/plugins/unfurl.cjs
@@ -1,5 +1,3 @@
-const path = require("path");
-const pluginImage = require("@11ty/eleventy-img");
 const pluginUnfurl = require("eleventy-plugin-unfurl");
 
 /** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
@@ -7,49 +5,28 @@ module.exports = (eleventyConfig) => {
 	eleventyConfig.addPlugin(pluginUnfurl, {
 		duration: "4w",
 		template: async (props) => {
-			const imageAttributes = {
-				alt: "",
-				class: "[ image unfurl__image ]",
-				decoding: "async",
-				loading: "lazy",
-				sizes: "(max-width: 768px) 100vw, 768px",
-			};
-
-			const metadata = props?.image?.url
-				? await pluginImage(props?.image?.url, {
-						cacheOptions: {
-							duration: "4w",
-						},
-						formats: ["avif", "webp", "jpeg"],
-						outputDir: path.join("_site", "img"),
-						widths: [300, 600, 1000],
-				  })
-				: {};
-
-			const image = props?.image?.url
-				? pluginImage.generateHTML(metadata, imageAttributes, {
-						whitespaceMode: "inline",
-				  })
-				: "";
-
 			return props
-				? `<article class="unfurl">${
+				? `<article class="unfurl"><p>${
 						props?.author
-							? `<small class="unfurl__meta"><span class="unfurl__publisher">${props.author}</span></small>`
+							? `<small class="unfurl-meta"><span class="unfurl-publisher">${props.author}</span></small>`
 							: ``
 				  }${
 						props?.url || props?.title
-							? `<span class="h4 unfurl__heading${
-									!props?.author ? ` unfurl__meta` : ``
-							  }"><a class="unfurl__link" href="${props?.url}">${
+							? `<span class="h4 unfurl-heading${
+									!props?.author ? ` unfurl-meta` : ``
+							  }"><a class="unfurl-link" href="${props?.url}">${
 									props?.title
 							  }</a></span>`
 							: ``
 				  }${
 						props?.description
-							? `<p class="unfurl__description">${props.description}</p>`
+							? `<span class="unfurl-description">${props.description}</span>`
 							: ``
-				  }${image}</article>`
+				  }${
+						props?.image?.url
+							? `<img alt="" class="[ image unfurl-image ]" decoding="async" loading="lazy" src="${props.image.url}" width="${props.image.width}" height="${props.image.height}">`
+							: ``
+				  }</p></article>`
 				: ``;
 		},
 	});
