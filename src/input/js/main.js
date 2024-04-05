09f6aff975280b86d9910255a7ace38c60fd42d9
Elly Loel <hello@ellyloel.com>
2024-04-05 14:46:34 +1100

    Removing shoelace, adding dompurify, and updating deps

--- src/input/js/main.js
+++ src/input/js/main.js
@@ -2,7 +2,6 @@ import "./color-scheme.js";
 import "./theme-hue.js";
 import "./color-scheme-switcher-dialog.js";
 import "./scrollbar-width.js";
-import "./shoelace.js";
 import "./details-utils.js";
 import "./css-doodle.js";
 import "./howler.js";
