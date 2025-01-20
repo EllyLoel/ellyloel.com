b0bbce2f10b22e1b342652fa9925a902a5d14bda
Elly Loel <hello@ellyloel.com>
2025-01-21 01:24:41 +1100

    Updating search

--- src/input/js/search.js
+++ src/input/js/search.js
@@ -1,31 +1,53 @@
-class Search {
+class PagefindSearch extends HTMLElement {
+	static register(tagName) {
+		if ("customElements" in window) {
+			customElements.define(tagName || "pagefind-search", PagefindSearch);
+		}
+	}
+
+	constructor() {
+		super();
+		this.pagefind = null;
+		this.onInputTimeout = null;
+	}
+
 	clearResults() {
-		this.searchResultsCount.innerHTML = "Results";
+		this.searchResultsCount.innerHTML = "results";
 		this.searchResultsList.innerHTML = "";
 	}
 
 	addResult(result) {
 		const listItem = document.createElement("li");
+		const title = document.createElement("h3");
 		const link = document.createElement("a");
-		const title = document.createElement("strong");
-		const matches = document.createElement("code");
+		const metadata = document.createElement("p");
+		const matches = document.createElement("p");
+
+		listItem.className = "[ flow ]";
 
 		link.href = result.url;
-		link.className = "search-results-item";
+		link.textContent = result.meta.title || result.url;
 
-		title.className = "search-results-item-title";
-		title.textContent = result.meta.title || result.url;
+		metadata.className = "[ metadata ]";
+		if (result.meta.date) {
+			const date = document.createElement("time");
+			date.dateTime = result.meta.date;
+			date.textContent = new Date(result.meta.date).toLocaleDateString("en-AU", { timeZone: "Australia/Melbourne" });
+			metadata.append(date);
+		}
+		if (result.filters.tags?.length) {
+			const tags = document.createElement("span");
+			tags.textContent = `Tags: ${result.filters.tags.join(", ")}`;
+			if (metadata.hasChildNodes()) {
+				metadata.append(" • ");
+			}
+			metadata.append(tags);
+		}
 
-		matches.className = "search-results-item-matches truncate-multiple-lines";
-		matches.innerHTML =
-			"&mldr;" +
-			result.excerpt
-				.replace(/</g, "&lt;")
-				.replace(/&lt;mark>/g, "<mark>")
-				.replace(/&lt;\/mark>/g, "</mark>");
+		matches.innerHTML = result.excerpt;
 
-		link.append(title, matches);
-		listItem.append(link);
+		title.append(link);
+		listItem.append(title, metadata, matches);
 		this.searchResultsList.append(listItem);
 	}
 
@@ -51,18 +73,20 @@ class Search {
 				for (let result of results) {
 					this.addResult(result, value);
 				}
+
 				if (results.length) {
-					this.searchResultsCount.innerHTML = `${results.length} Result${
+					this.searchResultsCount.innerHTML = `${results.length} result${
 						results.length > 1 ? "s" : ""
 					}`;
 				} else {
-					this.searchResultsList.innerHTML = `<li>No Matches Found.</li>`;
+					this.searchResultsList.innerHTML = `<li>No matches found.</li>`;
 				}
+
 				this.searchResultsList.classList[results.length > 0 ? "remove" : "add"](
 					"search-results-notfound"
 				);
 			} else {
-				this.searchResults.setAttribute("hidden");
+				this.searchResults.setAttribute("hidden", "");
 			}
 		}, 100);
 	}
@@ -73,33 +97,21 @@ class Search {
 		return searchQueryParam ? decodeURIComponent(searchQueryParam) : "";
 	}
 
-	hydrate() {
-		let form = document.getElementById("search");
+	connectedCallback() {
+		let form = this.querySelector(`form[role="search"]`);
 		if (form) {
-			form.addEventListener(
-				"submit",
-				function (event) {
-					event.preventDefault();
-				},
-				false
-			);
+			form.addEventListener("submit", (event) => event.preventDefault());
 		}
 
-		let text = document.getElementById("search-term");
+		let text = this.querySelector(`input[type="text"]`);
 		if (text) {
-			text.addEventListener(
-				"input",
-				async (event) => {
-					let value = event.target.value;
-					await this.onInput(value);
-					window.history.replaceState(
-						{},
-						"",
-						`/search/${value ? `?q=${encodeURIComponent(value)}` : ""}`
-					);
-				},
-				false
-			);
+			text.addEventListener("input", async (event) => {
+				let value = event.target.value;
+				await this.onInput(value);
+				window.history.replaceState(
+					{}, "", `/search/${value ? `?q=${encodeURIComponent(value)}` : ""}`
+				);
+			});
 
 			let queryString = this.getQueryString();
 			if (queryString) {
@@ -110,22 +122,21 @@ class Search {
 			}
 		}
 
-		let results = document.getElementById("search-results");
+		let results = document.querySelector(`#search-results`);
 		if (results) {
 			this.searchResults = results;
 		}
 
-		let resultsList = document.getElementById("search-results-list");
-		if (resultsList) {
-			this.searchResultsList = resultsList;
-		}
-
-		let resultsCount = document.getElementById("search-results-count");
+		let resultsCount = this.searchResults.querySelector(`h2`);
 		if (resultsCount) {
 			this.searchResultsCount = resultsCount;
+		}
+
+		let resultsList = this.searchResults.querySelector(`ol`);
+		if (resultsList) {
+			this.searchResultsList = resultsList;
 		}
 	}
 }
 
-let search = new Search();
-search.hydrate();
+PagefindSearch.register();
