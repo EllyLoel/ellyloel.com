63fe0f555d2e244182ab123a623a652fc819a4ed
Elly Loel <hello@ellyloel.com>
2025-03-11 12:49:44 +1100

    Fixing bookmark content missing from feeds

--- src/_11ty/filters/getYouTubeId.js
+++ src/_11ty/filters/getYouTubeId.js
@@ -1,17 +1,19 @@
 /* eslint-disable no-useless-escape */
 export default (url) => {
-	let id = "";
+	if (!url) return "";
+
+	let ytId = "";
 
 	url = url
 		.replace(/(>|<)/gi, "")
 		.split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
 
 	if (url[2] !== undefined) {
-		id = url[2].split(/[^0-9a-z_\-]/i);
-		id = id[0];
+		ytId = url[2].split(/[^0-9a-z_\-]/i);
+		ytId = ytId[0];
 	} else {
-		id = url;
+		ytId = url;
 	}
 
-	return id;
+	return ytId;
 };
