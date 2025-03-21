01fc3761cd485464c4977f28d29aa967fba96c2a
Elly Loel <hello@ellyloel.com>
2025-03-21 13:23:58 +1100

    Improving footer

--- src/input/js/css-doodle.js
+++ src/input/js/css-doodle.js
@@ -1,1 +1,8 @@
 import "css-doodle";
+
+const updateDoodleButton = document.querySelector("#update-doodle");
+const doodle = document.querySelector("css-doodle");
+
+updateDoodleButton.addEventListener("click", () => {
+	doodle.update();
+});
